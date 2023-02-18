739576bfe5ca1828152c38503a48dbbc5c48dca3
Boryan Hovo <bonnyluxe777@mail.ru>
2023-02-18 19:02:44 +0400

    I add new Testsetfood()

--- Tests.js
+++ Tests.js
@@ -5,10 +5,20 @@ const Bank = require('./Bank.js');
 const MenuAm = require('./MenuAm.js')
 
 
+function TestMenusetfood() {
+    const NewOrder = new MenuAm();
+    NewOrder.setFood(Food.xorovac);
+    if(NewOrder.getFood() !== "xorovac") {
+        console.log("Test setFood() failed !!!..");
+    } else {
+        console.log("Test setFood() passed .");
+    }
+}
+
 function TestSetForCoocking() {
  const Parvana = new Restaurant(Food.xorovac);
  Parvana.setForCoocking(Food.xorovac);
-    if(Parvana.getCoockingFood() !== Food.xorovac ) {
+    if(Parvana.getCoockingFood() !== "xorovac" ) {
         console.log("Test takeorder() failed !!!..");
         } else {
         console.log("Test takeOrder() passed.");
@@ -25,7 +35,7 @@ function TestgetFoodforDelivery() {
    
     const shipper = new Shipper();
     shipper.setFoodforDelivery(Food.xorovac);
-    if(shipper.getFoodforDelivery() !== Food.xorovac) {
+    if(shipper.getFoodforDelivery() !== "xorovac") {
         console.log("Test getFoodforDelivery() failed !!!..");
     } else {
         console.log("Test getFoodforDelivery() passed .");
@@ -43,7 +53,7 @@ function TestgetFoodforDelivery() {
 //TestMenuendDelivery()
 
 function TestAlltests() {
-
+    TestMenusetfood()
     TestSetForCoocking()
     TestgetFoodforDelivery()
 }
