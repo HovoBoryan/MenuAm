e4fea2a1ce1ff2acf9560eaefcba7491a85f7366
Boryan Hovo <bonnyluxe777@mail.ru>
2023-02-18 11:03:51 +0400

    I add new file Tests.js

---
+++ Tests.js
@@ -0,0 +1,23 @@
+const Restaurant = require('./Restaurant');
+const Shipper = require('./shipper');
+const User = require('./client.js');
+const Bank = require('./Bank.js');
+const MenuAm = require('./MenuAm.js')
+
+
+TesttakeOrder(orderfood)
+
+TestgiveFoodShipper(food) 
+
+TestgetFoodforDelivery(food) 
+
+TestendDelivery() 
+
+TestMenusetFood(food)
+
+TestMenugetFood()
+
+TestMenugiveOrder(clientname, food)
+
+TestMenuendDelivery()
+
